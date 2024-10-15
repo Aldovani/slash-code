6ba4226e21f51f34fe86ddba649110e5bf1451d3
Aldovani <aldovanihcosta@gmail.com>
2024-10-15 15:14:00 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+       {hostname:'sa-east-1.graphassets.com'},
+       {hostname:'avatars.githubusercontent.com'}
+     ]
+  }
+};
 
 export default nextConfig;
