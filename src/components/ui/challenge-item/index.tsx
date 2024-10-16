b905c607304210245ec39b2d3ea184b8f9b8199a
Aldovani <aldovanihcosta@gmail.com>
2024-10-16 16:59:40 -0300

    fix: styles

--- src/components/ui/challenge-item/index.tsx
+++ src/components/ui/challenge-item/index.tsx
@@ -11,12 +11,12 @@ export function ChallengeItem({ data }: ChallengeItemProps) {
       href={`/challenges/${data.slug}`}
       className="group p-4 rounded-lg border border-slate-200  bg-white  w-full transition-all hover:shadow-2xl hover:shadow-slate-200 "
     >
-      <div className="relative h-[220px]  w-full object-fill rounded-t-lg overflow-hidden">
+      <div className="relative h-[220px]  w-full object-fill rounded-lg overflow-hidden">
         <Image
           fill
           alt={`challenge ${data.title} preview`}
           src={data.preview}
-          className=" group-hover:scale-105 block h-full absolute inset-0 transition-transform w-full"
+          className=" group-hover:scale-105 block h-full object-cover absolute inset-0 transition-transform w-full"
         />
       </div>
       <div className=" flex justify-between flex-wrap mt-5">
@@ -30,7 +30,7 @@ export function ChallengeItem({ data }: ChallengeItemProps) {
           </Badge>
         </div>
       </div>
-      <p className="max-w-[336px] text-sm  text-slate-400 line-clamp-2	mt-2">
+      <p className="max-w-[336px] text-sm  text-slate-400 	mt-4">
         {data.description}
       </p>
     </Link>
