ca02a341248f6eb0ba5eb6c8435bcee56ac26068
Aldovani <aldovanihcosta@gmail.com>
2024-10-16 16:56:54 -0300

    fix: remove link

--- src/app/(home)/components/features/cards/challenges/challenge-item.tsx
+++ src/app/(home)/components/features/cards/challenges/challenge-item.tsx
@@ -1,6 +1,4 @@
 import Image, { StaticImageData } from "next/image";
-import Link from "next/link";
-
 type ChallengeItem = {
   imgUrl: StaticImageData;
   name: string;
@@ -8,12 +6,11 @@ type ChallengeItem = {
 
 export function ChallengeItem({ imgUrl, name }: ChallengeItem) {
   return (
-    <Link
-      href="/"
-     title={`challenge ${name}`}
+    <div
+      title={`challenge ${name}`}
       className="border w-[285px] overflow-hidden  bg-white rounded-lg h-[215px] p-2 border-slate-200"
     >
       <Image alt={name} src={imgUrl} className="rounded-md" />
-    </Link>
+    </div>
   );
 }
