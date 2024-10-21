a2f6c9e0f2cee4f367203e4bd552945c396f08e4
Aldovani <aldovanihcosta@gmail.com>
2024-10-21 12:42:26 -0300

    fix: remove challenges.data file and add mock to test

--- src/app/(home)/components/how-it-works/how-it-works.spec.tsx
+++ src/app/(home)/components/how-it-works/how-it-works.spec.tsx
@@ -1,11 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import { HowItWorks } from ".";
-import { CHALLENGES } from "../browser/components/web-site/challenges.data";
+import { Area, Difficult } from "@/graphql/generated/graphql";
+import { challenges } from "@/types/challenges";
 
 jest.mock("../browser/components/visual-studio-code", () => {
   const ComponentToMock = () => <div />;
   return { VisualStudioCode: ComponentToMock };
 });
+
+export const CHALLENGES: challenges[] = [
+  {
+    area: Area.FrontEnd,
+    description:
+      "Yorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. ",
+    difficult: Difficult.Iniciante,
+    preview: {
+      url: "http://localhost:3000/_next/image?url=https%3A%2F%2Fsa-east-1.graphassets.com%2Fclyrig3d502eg07knb4q44aq6%2Fcm2at9nid0ck707kb8guyr4zn&w=1920&q=75",
+    },
+    slug: "ibico",
+    title: "Ibico",
+    id: "ibico",
+  },
+];
 
 describe("<HowItWorks/>", () => {
   it("should be able to render the component", () => {
