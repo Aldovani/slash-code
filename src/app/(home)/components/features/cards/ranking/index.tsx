5cd42d7e3a141ae3e3845493bd586249effb2569
Aldovani <aldovanihcosta@gmail.com>
2024-10-24 09:43:35 -0300

    fix: remove motion div and add to motion to component

--- src/app/(home)/components/features/cards/ranking/index.tsx
+++ src/app/(home)/components/features/cards/ranking/index.tsx
@@ -19,8 +19,8 @@ export function Ranking() {
           desenvolvedores. Quanto mais você avança, maior será sua conquista.
         </Card.Description>
       </Card.Header>
-      <Card.Content className=" overflow-hidden relative flex justify-center items-center pt-10 sm:px-5 px-2">
-        <motion.div
+      <Card.Content className=" overflow-hidden relative flex justify-center items-center pt-10 sm:px-5 px-5">
+        <RankCard
           initial={{
             translateX: -100,
             translateY: 90,
@@ -34,56 +34,47 @@ export function Ranking() {
             opacity: 1,
           }}
           transition={{ delay: 1, duration: 0.5 }}
-          animate
-          className="absolute top-0 sm:block hidden"  
+          className="absolute top-0 sm:block hidden"
         >
-          <RankCard>
-            <RankItem
-              name="Julio Alexandre das Neves"
-              position={5}
-              score={1500}
-            />
-            <RankItem
-              name="Evelyn Mariana Aline Mendes"
-              position={6}
-              score={800}
-            />
-            <RankItem
-              name="Leandro Miguel Ruan Monteiro"
-              position={6}
-              score={550}
-            />
-            <RankItem name="Leonardo Cauã Novaes" position={8} score={300} />
-          </RankCard>
-        </motion.div>
+          <RankItem
+            name="Julio Alexandre das Neves"
+            position={5}
+            score={1500}
+          />
+          <RankItem
+            name="Evelyn Mariana Aline Mendes"
+            position={6}
+            score={800}
+          />
+          <RankItem
+            name="Leandro Miguel Ruan Monteiro"
+            position={6}
+            score={550}
+          />
+          <RankItem name="Leonardo Cauã Novaes" position={8} score={300} />
+        </RankCard>
 
-        <motion.div
+        <RankCard
           initial={{ translateY: 100, opacity: 0 }}
-          whileInView={{ translateY: 20, opacity: 1}}
+          whileInView={{ translateY: 20, opacity: 1 }}
           transition={{ duration: 0.5 }}
-          className="relative z-[2] bg-white shadow-2xl shadow-slate-300  "
+          className="z-[2]"
         >
-          <RankCard>
-            <RankItem
-              name="Jaqueline Ayla de Paula"
-              position={1}
-              score={1500}
-            />
-            <RankItem
-              name="Manuela Brenda Tânia Assunção"
-              position={2}
-              score={800}
-            />
-            <RankItem name="Stefany Juliana Lima" position={3} score={550} />
-            <RankItem
-              name="Cecília Silvana Simone Oliveira"
-              position={4}
-              score={300}
-            />
-          </RankCard>
-        </motion.div>
+          <RankItem name="Jaqueline Ayla de Paula" position={1} score={1500} />
+          <RankItem
+            name="Manuela Brenda Tânia Assunção"
+            position={2}
+            score={800}
+          />
+          <RankItem name="Stefany Juliana Lima" position={3} score={550} />
+          <RankItem
+            name="Cecília Silvana Simone Oliveira"
+            position={4}
+            score={300}
+          />
+        </RankCard>
 
-        <motion.div
+        <RankCard
           className="absolute top-0  sm:block hidden "
           initial={{ translateX: 100, translateY: 90, rotate: 12, opacity: 0 }}
           whileInView={{
@@ -93,23 +84,20 @@ export function Ranking() {
             opacity: 1,
           }}
           transition={{ delay: 1, duration: 0.5 }}
-          animate
         >
-          <RankCard>
-            <RankItem name="Jaqueline Ayla de Paula" position={1} score={200} />
-            <RankItem
-              name="Manuela Brenda Tânia Assunção"
-              position={2}
-              score={180}
-            />
-            <RankItem name="Stefany Juliana Lima" position={3} score={100} />
-            <RankItem
-              name="Cecília Silvana Simone Oliveira"
-              position={4}
-              score={70}
-            />
-          </RankCard>
-        </motion.div>
+          <RankItem name="Jaqueline Ayla de Paula" position={1} score={200} />
+          <RankItem
+            name="Manuela Brenda Tânia Assunção"
+            position={2}
+            score={180}
+          />
+          <RankItem name="Stefany Juliana Lima" position={3} score={100} />
+          <RankItem
+            name="Cecília Silvana Simone Oliveira"
+            position={4}
+            score={70}
+          />
+        </RankCard>
       </Card.Content>
     </Card.Root>
   );
