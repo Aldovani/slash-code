058f6419d0ad6220da65e32388b62ccbaf2d9637
Aldovani <aldovanihcosta@gmail.com>
2024-10-24 09:42:50 -0300

    fix: props

--- src/app/challenges/[slug]/components/header/index.tsx
+++ src/app/challenges/[slug]/components/header/index.tsx
@@ -16,41 +16,36 @@ type Author = {
 };
 
 type ChallengeHeaderProps = {
-  area: Area;
-  difficult: string;
-  description: string;
-  title: string;
-  figmaUrl: string | null | undefined;
-  author: Author;
+  data: {
+    area: Area;
+    difficult: string;
+    description: string;
+    title: string;
+    figmaUrl: string | null | undefined;
+    author: Author;
+  };
 };
 
-export function ChallengeHeader({
-  area,
-  difficult,
-  description,
-  figmaUrl,
-  title,
-  author,
-}: ChallengeHeaderProps) {
+export function ChallengeHeader({ data }: ChallengeHeaderProps) {
   return (
     <header className="mt-6 flex max-md:flex-col max-md:gap-8   justify-between md:items-center">
       <div>
         <div>
           <div className="flex gap-2 items-center">
-            <Badge>{RewriteArea(area)}</Badge>
-            <Badge>{difficult}</Badge>
+            <Badge>{RewriteArea(data.area)}</Badge>
+            <Badge>{data.difficult}</Badge>
           </div>
-          <h1 className="text-5xl text-slate-600 mt-3 font-semibold ">
-            {title}
+          <h1 className="sm:text-5xl text-3.5xl text-slate-600 mt-3 font-semibold ">
+            {data.title}
           </h1>
-          <p className="break-words text-slate-500 max-w-[650px] mt-2 line-clamp-2 ">
-            {description}
+          <p className="break-words text-slate-400 max-w-[650px] mt-2 md:line-clamp-2 sm:text-base  text-xs">
+            {data.description}
           </p>
         </div>
 
-        {figmaUrl && (
+        {data.figmaUrl && (
           <a
-            href={figmaUrl}
+            href={data.figmaUrl}
             target="_blank"
             className="w-full bg-violet-600 text-white max-w-[380px] py-2 rounded-md block text-center mt-6 hover:bg-violet-500 transition-colors"
           >
@@ -64,36 +59,39 @@ export function ChallengeHeader({
         <div className="flex  items-start mt-2 gap-3">
           <Image
             alt="Aldovani"
-            src={author.avatar || ""}
+            src={data.author.avatar || ""}
             width={64}
             height={64}
             className="rounded-full"
           />
 
           <div>
-            <h3 className="font-semibold text-slate-600">{author.name}</h3>
-            <span className=" text-slate-500">{author.role}</span>
+            <h3 className="font-semibold text-slate-600">{data.author.name}</h3>
+            <span className=" text-slate-500">{data.author.role}</span>
 
             <nav className="mt-2">
               <ul className="flex gap-3">
-                {author.social.portfolio && (
+                {data.author.social.portfolio && (
                   <li className="text-slate-500 hover:text-violet-600">
-                    <a href={author.social.portfolio} target="_blank">
+                    <a href={data.author.social.portfolio} target="_blank">
                       <Smile />
+                      <span className="sr-only">Portfolio</span>
                     </a>
                   </li>
                 )}
-                {author.social.linkedin && (
+                {data.author.social.linkedin && (
                   <li className="text-slate-500 hover:text-violet-600">
-                    <a href={author.social.linkedin} target="_blank">
+                    <a href={data.author.social.linkedin} target="_blank">
                       <Linkedin />
+                      <span className="sr-only">Linkedin</span>
                     </a>
                   </li>
                 )}
-                {author.social.twitter && (
+                {data.author.social.twitter && (
                   <li className="text-slate-500 hover:text-violet-600">
-                    <a href={author.social.twitter} target="_blank">
+                    <a href={data.author.social.twitter} target="_blank">
                       <Twitter />
+                      <span className="sr-only">Twitter</span>
                     </a>
                   </li>
                 )}
