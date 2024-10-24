e87a4965887ddbcb98f36838891a9c6891769670
Aldovani <aldovanihcosta@gmail.com>
2024-10-24 09:38:45 -0300

    fix: change seo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,12 +9,13 @@ import {
 import { CallToAction } from "@/components/ui/call-to-action";
 import { Faq } from "@/components/ui/faq";
 import type { Metadata } from "next";
-import { HomePageMetaTag } from "./home.meta";
 import { getChallenges } from "@/services/challenegs";
 
 export const revalidate = 604800;
 
-export const metadata: Metadata = HomePageMetaTag;
+export const metadata: Metadata = {
+  title: "Home - SlashCode",
+};
 
 export default async function HomePage() {
   const challenges = await getChallenges({ perPage: 6 });
